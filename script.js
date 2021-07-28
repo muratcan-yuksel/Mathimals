623ea9f0784353eb5f4e426ac81041500d0aa1d3
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-07-28 18:14:08 +0300

    added addition function, it's not DRY, but I'm too lazy to think of sth else

--- script.js
+++ script.js
@@ -17,7 +17,95 @@ const gamePlayModule = (() => {
   // createSumNumber(5);
   console.log(dom.givenNumber);
 
-  const shuffle = () => {
+  const addition = () => {
+    //array that holds the possible numbers that when added can give the correct answer
+    let possibleNumbers = [];
+    const createNumbers = () => {
+      for (i = 1; i <= dom.givenNumber.textContent; i++) {
+        possibleNumbers.push(i);
+      }
+    };
+
+    createNumbers();
+    console.log(possibleNumbers);
+
+    // //slice the last element of the array so that the answer cannot be "1 * the given number" all the time
+    let poppedNumbers = possibleNumbers.slice(0, -1);
+    console.log(poppedNumbers);
+
+    let sum = Number(dom.givenNumber.textContent);
+    // console.log(sum);
+    console.log(dom.givenNumber.textContent);
+
+    document.getElementById("number").textContent = sum;
+
+    //logic that gives the two numbers that gives the correct answer when multiplied to each other
+    function detectPair(sum, poppedNumbers) {
+      for (i = 0; i < poppedNumbers.length; i++) {
+        for (j = 0; j < poppedNumbers.length; j++) {
+          if (i == j) continue;
+          else if (poppedNumbers[i] + poppedNumbers[j] === sum)
+            return [poppedNumbers[i], poppedNumbers[j]];
+        }
+      }
+      return null;
+    }
+
+    console.log(poppedNumbers[poppedNumbers.length - 1]);
+
+    console.log(detectPair(sum, poppedNumbers));
+    console.log(detectPair(sum, possibleNumbers));
+
+    //function to get a random element from an array
+    const getRandomElement = (array) => {
+      return array[Math.floor(Math.random() * array.length)];
+    };
+    //get a random element from the card operations array
+    const correctAnswer = getRandomElement(dom.cardOperations);
+    console.log(correctAnswer);
+    //remove its class
+    correctAnswer.classList.remove("card-operation");
+    //add a new one
+    correctAnswer.classList.add("correct-answer");
+
+    //generate a random operation between numbers in the array on start game button click
+    const generateOperation = () => {
+      dom.cardOperations.forEach((card) => {
+        if (card.className != "correct-answer") {
+          card.textContent =
+            Math.floor(Math.random() * dom.givenNumber.textContent) +
+            1 +
+            "+" +
+            (Math.floor(Math.random() * dom.givenNumber.textContent) + 1);
+        } else {
+          /*these if statements prevent the case in which the number has no divisors in poppedNumbers, 
+but obviously have in possibleNumbers( like 1 and the number itself)  */
+          if (detectPair(sum, poppedNumbers) == null) {
+            card.textContent =
+              detectPair(sum, possibleNumbers)[0] +
+              "+" +
+              detectPair(sum, possibleNumbers)[1];
+            correctAnswer.classList.remove("correct-answer");
+            correctAnswer.classList.add("card-operation");
+          } else {
+            card.textContent =
+              detectPair(sum, poppedNumbers)[0] +
+              "+" +
+              detectPair(sum, poppedNumbers)[1];
+            correctAnswer.classList.remove("correct-answer");
+            correctAnswer.classList.add("card-operation");
+          }
+        }
+      });
+    };
+    //call the function, if not, the cards will not have numbers on them
+    generateOperation();
+    //empty the arrays so that they won't stack up
+    possibleNumbers = [];
+    poppedNumbers = [];
+  };
+
+  const multiplication = () => {
     //array that holds the possible numbers that when multiplied can give the correct answer
     let possibleNumbers = [];
     const createNumbers = () => {
@@ -105,26 +193,34 @@ const gamePlayModule = (() => {
     poppedNumbers = [];
   };
   //return only the function(s) needed
-  return { shuffle, createSumNumber };
+  return { multiplication, createSumNumber };
 })();
 
 dom.startBtn.addEventListener("click", () => {
   dom.innerCards.forEach((card) => {
     card.style.transform = "rotateY(180deg)";
   });
-  //call the shuffle function
+  //call the multiplication function
   gamePlayModule.createSumNumber(10);
-  gamePlayModule.shuffle();
+  gamePlayModule.multiplication();
 });
+/*will use this array to change levels
+how?
+if the array length is greater than 4, change to division for instance
+if it's greater than 8, change to addition and so on */
+let arr = [];
 
 //when the next level button is clicked, the game will be played again
 dom.nextLevelButton.addEventListener("click", () => {
+  //push something to array on each click
+  arr.push(1);
+  console.log(arr);
   dom.innerCards.forEach((card) => {
     card.style.transform = "rotateY(180deg)";
   });
-  //call the shuffle function
+  //call the multiplication function
   //The only problem is that when the number is big, it goes outside of the card.
   //The solution: Make the font smaller.
   gamePlayModule.createSumNumber(200);
-  gamePlayModule.shuffle();
+  gamePlayModule.multiplication();
 });
