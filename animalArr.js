638f9a9f0307360c6eeed991845d4c162930c83d
annagiuliam <annagiulia.musso@gmail.com>
2021-07-30 18:00:24 +0200

    renamed pics

---
+++ animalArr.js
@@ -0,0 +1,82 @@
+export const animalArr = [
+  {
+    imageSrc: "bear.jpg",
+    nameSrc: "bear-name.jpg",
+    nameString: "bear",
+  },
+  {
+    imageSrc: "cat.jpg",
+    nameSrc: "cat-name.jpg",
+    nameString: "cat",
+  },
+  {
+    imageSrc: "dinosaur.jpg",
+    nameSrc: "dinosaur-name.jpg",
+    nameString: "dinosaur",
+  },
+  {
+    imageSrc: "giraffe.jpg",
+    nameSrc: "giraffe-name.jpg",
+    nameString: "giraffe",
+  },
+  {
+    imageSrc: "goat.jpg",
+    nameSrc: "goat-name.jpg",
+    nameString: "goat",
+  },
+  {
+    imageSrc: "hippo.jpg",
+    nameSrc: "hippo-name.jpg",
+    nameString: "hippo",
+  },
+  {
+    imageSrc: "kangaroo.jpg",
+    nameSrc: "kangaroo-name.jpg",
+    nameString: "kangaroo",
+  },
+  {
+    imageSrc: "monkey.jpg",
+    nameSrc: "monkey-name.jpg",
+    nameString: "monkey",
+  },
+  {
+    imageSrc: "owl.jpg",
+    nameSrc: "owl-name.jpg",
+    nameString: "owl",
+  },
+  {
+    imageSrc: "panda.jpg",
+    nameSrc: "panda-name.jpg",
+    nameString: "panda",
+  },
+  {
+    imageSrc: "penguin.jpg",
+    nameSrc: "penguin-name.jpg",
+    nameString: "penguin",
+  },
+  {
+    imageSrc: "pig.jpg",
+    nameSrc: "pig-name.jpg",
+    nameString: "pig",
+  },
+  {
+    imageSrc: "rhino.jpg",
+    nameSrc: "rhino-name.jpg",
+    nameString: "rhino",
+  },
+  {
+    imageSrc: "snake.jpg",
+    nameSrc: "snake-name.jpg",
+    nameString: "snake",
+  },
+  {
+    imageSrc: "tiger.jpg",
+    nameSrc: "tiger-name.jpg",
+    nameString: "tiger",
+  },
+  {
+    imageSrc: "zebra.jpg",
+    nameSrc: "zebra-name.jpg",
+    nameString: "zebra",
+  },
+];
