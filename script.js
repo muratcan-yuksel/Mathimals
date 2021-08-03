b094d90f50e91946d5a5ea51cda3c3ec7c5080cf
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-04 02:06:10 +0300

    the same solution for division

--- script.js
+++ script.js
@@ -432,8 +432,12 @@ but obviously have in possibleNumbers( like 1 and the number itself)  */
       for (let i = 0; i < poppedNumbers.length; i++) {
         for (let j = 0; j < poppedNumbers.length; j++) {
           if (i == j) continue;
-          else if (poppedNumbers[i] / poppedNumbers[j] === sum)
+          else if (poppedNumbers[i] / poppedNumbers[j] === sum){
+            sessionStorage.setItem("firstDivisionNumber", poppedNumbers[i])
+            sessionStorage.setItem("secondDivisionNumber", poppedNumbers[j])
             return [poppedNumbers[i], poppedNumbers[j]];
+
+          }
         }
       }
       return null;
@@ -458,16 +462,26 @@ but obviously have in possibleNumbers( like 1 and the number itself)  */
 
     sessionStorage.setItem("answer", correctAnswer.parentNode.children[2].alt);
 
+     //get the numbers you'll exclude from the random (wrong) answers
+     let excludeOne= sessionStorage.getItem("firstDivisionNumber")
+     let excludeTwo = sessionStorage.getItem("secondDivisionNumber")
+
+     function generateWrongAnswers (min, max) {
+      let num = Math.floor(Math.random() * (max - min + 1)) + min;
+      return (num === excludeOne || num === excludeTwo) ? generateRandom(min, max) : num;
+    }
+    
+
     //generate a random operation between numbers in the array on start game button click
     const generateOperation = () => {
       dom.cardOperations.forEach((card) => {
         if (card.className != "correct-answer") {
           card.textContent =
-            Math.floor(Math.random() * dom.givenNumber.textContent) +
-            1 +
+          generateWrongAnswers(1, 250) +
+
             "/" +
-            (Math.floor(Math.random() * dom.givenNumber.textContent) + 1);
-        } else {
+            generateWrongAnswers(1, 250) 
+          } else {
           /*these if statements prevent the case in which the number has no divisors in poppedNumbers, 
     but obviously have in possibleNumbers( like 1 and the number itself)  */
           if (detectPair(sum, poppedNumbers) == null) {
